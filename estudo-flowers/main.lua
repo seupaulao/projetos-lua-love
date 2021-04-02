d10d8aa7a4c3b5f8f61a27aebead14f4200c4629
81784244368 <paulo.silva-junior@serpro.gov.br>
2021-04-02 09:16:05 -0300

    alterados, ecluidos e modificados

---
+++ estudo-flowers/main.lua
@@ -0,0 +1,17 @@
+function love.load()
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key)
+    if key == 'escape' then 
+         love.event.quit()
+    end
+end
+
+function love.update(dt)
+
+end
