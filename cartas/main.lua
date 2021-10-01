718fd5c2d94b995563b8c43e0c78cd6de6bdfe32
81784244368 <paulo.silva-junior@serpro.gov.br>
2021-10-01 08:56:07 -0300

    inserindo elementos gui das telas novas

--- cartas/main.lua
+++ cartas/main.lua
@@ -20,6 +20,9 @@ function love.load()
 
   baralhos = {}
 
+  gui = {}
+  table.insert(gui, love.graphics.newImage('gui/'..'seta-direita-verde.png'))
+
   table.insert(baralhos, Baralho(criarDeque()))
   --deque = criarDeque()
   --deque = embaralhar(deque)
@@ -27,6 +30,7 @@ function love.load()
   indice = 1
   numDeques = 1
   baralhos[indice]:setCartas(embaralhar(baralhos[indice]:getCartas()))
+  telaAtual = 1
   --print(baralhos[indice]:qte())
   --print(baralhos[indice]:cont())
   --printCards(baralhos[indice]:getCartas())
@@ -43,151 +47,33 @@ function love.keypressed(key)
     baralhos[indice]:rem()
   elseif key == 'e' then
     baralhos[indice]:setCartas(embaralhar(baralhos[indice]:getCartas()))
-    --printCards(baralhos[indice]:getCartas())
     baralhos[indice]:zerar()
+  elseif key == 't' then
+    telaAtual = telaAtual + 1
+    if telaAtual > 8 then 
+      telaAtual = 1
+    end  
   end
 end
 
 
 function love.draw()
-
-    local function drawCard(card, x, y)
-        love.graphics.setColor(1, 1, 1)
-        love.graphics.draw(images.card, x, y)
-
-        if card.suit == 'heart' or card.suit == 'diamond' then
-          love.graphics.setColor(.89, .06, .39)
-        else
-          love.graphics.setColor(.2, .2, .2)
-        end
-
-        local cardWidth = 53
-        local cardHeight = 73
-
-        local function drawCorner(image, offsetX, offsetY)
-          love.graphics.draw(
-              image,
-              x + offsetX,
-              y + offsetY
-          )
-          love.graphics.draw(
-              image,
-              x + cardWidth - offsetX,
-              y + cardHeight - offsetY,
-              0,
-              -1
-          )
-        end
-
-      drawCorner(images[card.rank], 3, 4)
-      drawCorner(images['mini_'..card.suit], 3, 14)
-
-      if card.rank > 10 then
-          local faceImage
-          if card.rank == 11 then 
-            faceImage = images.face_jack
-          elseif card.rank == 12 then 
-            faceImage = images.face_queen  
-          elseif card.rank == 13 then 
-            faceImage = images.face_king
-          end
-          love.graphics.setColor(1,1,1)
-          love.graphics.draw(faceImage,x+12,y+11)  
-      else
-            local function drawPip(offsetX, offsetY, mirrorX, mirrorY)
-              local pipImage = images['pip_'..card.suit]
-              local pipWidth = 11
-
-              love.graphics.draw(
-                  pipImage,
-                  x + offsetX,
-                  y + offsetY
-              )
-              if mirrorX then
-                  love.graphics.draw(
-                      pipImage,
-                      x + cardWidth - offsetX - pipWidth,
-                      y + offsetY
-                  )
-              end
-              if mirrorY then
-                  love.graphics.draw(
-                      pipImage,
-                      x + offsetX + pipWidth,
-                      y + cardHeight - offsetY,
-                      0,
-                      -1
-                  )
-              end
-              if mirrorX and mirrorY then
-                  love.graphics.draw(
-                      pipImage,
-                      x + cardWidth - offsetX,
-                      y + cardHeight - offsetY,
-                      0,
-                      -1
-                  )
-              end
-          end
-
-          local xLeft = 11
-          local xMid = 21
-          local yTop = 7
-          local yThird = 19
-          local yQtr = 23
-          local yMid = 31
-
-          if card.rank == 1 then
-              drawPip(xMid, yMid)
-
-          elseif card.rank == 2 then
-              drawPip(xMid, yTop, false, true)
-
-          elseif card.rank == 3 then
-              drawPip(xMid, yTop, false, true)
-              drawPip(xMid, yMid)
-
-          elseif card.rank == 4 then
-              drawPip(xLeft, yTop, true, true)
-
-          elseif card.rank == 5 then
-              drawPip(xLeft, yTop, true, true)
-              drawPip(xMid, yMid)
-
-          elseif card.rank == 6 then
-              drawPip(xLeft, yTop, true, true)
-              drawPip(xLeft, yMid, true)
-
-          elseif card.rank == 7 then
-              drawPip(xLeft, yTop, true, true)
-              drawPip(xLeft, yMid, true)
-              drawPip(xMid, yThird)
-
-          elseif card.rank == 8 then
-              drawPip(xLeft, yTop, true, true)
-              drawPip(xLeft, yMid, true)
-              drawPip(xMid, yThird, false, true)
-
-          elseif card.rank == 9 then
-              drawPip(xLeft, yTop, true, true)
-              drawPip(xLeft, yQtr, true, true)
-              drawPip(xMid, yMid)
-
-          elseif card.rank == 10 then
-              drawPip(xLeft, yTop, true, true)
-              drawPip(xLeft, yQtr, true, true)
-              drawPip(xMid, 16, false, true)
-          end
-      end
-  end
-
-
-if  baralhos[indice]:cont() < baralhos[indice]:qte() then 
-    love.graphics.setColor(1, 1, 1)
-    love.graphics.draw(images.card_face_down, 60, 20)
-end
-
-drawCard(baralhos[indice]:getCarta(), 2 * 60, 20)
-
+  if telaAtual == 1 then 
+    telaAbertura()
+  elseif telaAtual == 2 then   
+    telaDefinirQuantidadeDeques()
+  elseif telaAtual == 3 then   
+    telaVisualizarDeques()
+  elseif telaAtual == 4 then   
+    telaModoJogo()
+  elseif telaAtual == 5 then   
+    telaPontuacao()
+  elseif telaAtual == 6 then   
+    telaAjuda()
+  elseif telaAtual == 7 then   
+    telaRemoveAds()
+  elseif telaAtual == 8 then   
+    telaDoacao()
+  end 
 
 end
--- cartas/telas.lua
+++ cartas/telas.lua
@@ -1,13 +1,13 @@
---1. criar tela abertura, 
+--1. criar tela abertura [IMG], 
 --         START
 --         HELP 
 --             tela de ajuda
 --             tela de ensinar tecnica de loci/palacio mental
---             tela de doação
+--             tela de doação 
 --             tela de requisitar AD em video
 --2. criar tela de pontuacao
 --                 pontuacao : usuario, qte deques, num total cartas, qte acertos cartas, qte erros cartas, tempo memorizacao
---3. criar tela de escolher a quantidade de deques
+--3. criar tela de escolher a quantidade de deques [IMG]
 --         numDeques
 --4. criar tela de visualizar as cartas/modo jogo
 --      até 8 deques por subtela, cada subtela eh uma pagina, mostrar qual pagina está sendo exibida
@@ -30,8 +30,9 @@ end
 
 function telaAbertura()
     love.graphics.print("CARD's MEMORY",100,100)
-    love.graphics.print("START",240,100)
-    love.graphics.print("HELP",360,100)
+    love.graphics.print("START",100,240)
+    love.graphics.print("HELP",100,360)
+    love.graphics.draw(gui[1],240,100)
 end
 
 function telaDefinirQuantidadeDeques()
@@ -40,19 +41,162 @@ function telaDefinirQuantidadeDeques()
 end
 
 function telaVisualizarDeques()
+            local function drawCard(card, x, y)
+                love.graphics.setColor(1, 1, 1)
+                love.graphics.draw(images.card, x, y)
 
+                if card.suit == 'heart' or card.suit == 'diamond' then
+                love.graphics.setColor(.89, .06, .39)
+                else
+                love.graphics.setColor(.2, .2, .2)
+                end
+
+                local cardWidth = 53
+                local cardHeight = 73
+
+                local function drawCorner(image, offsetX, offsetY)
+                love.graphics.draw(
+                    image,
+                    x + offsetX,
+                    y + offsetY
+                )
+                love.graphics.draw(
+                    image,
+                    x + cardWidth - offsetX,
+                    y + cardHeight - offsetY,
+                    0,
+                    -1
+                )
+                end
+
+            drawCorner(images[card.rank], 3, 4)
+            drawCorner(images['mini_'..card.suit], 3, 14)
+
+            if card.rank > 10 then
+                local faceImage
+                if card.rank == 11 then 
+                    faceImage = images.face_jack
+                elseif card.rank == 12 then 
+                    faceImage = images.face_queen  
+                elseif card.rank == 13 then 
+                    faceImage = images.face_king
+                end
+                love.graphics.setColor(1,1,1)
+                love.graphics.draw(faceImage,x+12,y+11)  
+            else
+                    local function drawPip(offsetX, offsetY, mirrorX, mirrorY)
+                    local pipImage = images['pip_'..card.suit]
+                    local pipWidth = 11
+
+                    love.graphics.draw(
+                        pipImage,
+                        x + offsetX,
+                        y + offsetY
+                    )
+                    if mirrorX then
+                        love.graphics.draw(
+                            pipImage,
+                            x + cardWidth - offsetX - pipWidth,
+                            y + offsetY
+                        )
+                    end
+                    if mirrorY then
+                        love.graphics.draw(
+                            pipImage,
+                            x + offsetX + pipWidth,
+                            y + cardHeight - offsetY,
+                            0,
+                            -1
+                        )
+                    end
+                    if mirrorX and mirrorY then
+                        love.graphics.draw(
+                            pipImage,
+                            x + cardWidth - offsetX,
+                            y + cardHeight - offsetY,
+                            0,
+                            -1
+                        )
+                    end
+                end
+
+                local xLeft = 11
+                local xMid = 21
+                local yTop = 7
+                local yThird = 19
+                local yQtr = 23
+                local yMid = 31
+
+                if card.rank == 1 then
+                    drawPip(xMid, yMid)
+
+                elseif card.rank == 2 then
+                    drawPip(xMid, yTop, false, true)
+
+                elseif card.rank == 3 then
+                    drawPip(xMid, yTop, false, true)
+                    drawPip(xMid, yMid)
+
+                elseif card.rank == 4 then
+                    drawPip(xLeft, yTop, true, true)
+
+                elseif card.rank == 5 then
+                    drawPip(xLeft, yTop, true, true)
+                    drawPip(xMid, yMid)
+
+                elseif card.rank == 6 then
+                    drawPip(xLeft, yTop, true, true)
+                    drawPip(xLeft, yMid, true)
+
+                elseif card.rank == 7 then
+                    drawPip(xLeft, yTop, true, true)
+                    drawPip(xLeft, yMid, true)
+                    drawPip(xMid, yThird)
+
+                elseif card.rank == 8 then
+                    drawPip(xLeft, yTop, true, true)
+                    drawPip(xLeft, yMid, true)
+                    drawPip(xMid, yThird, false, true)
+
+                elseif card.rank == 9 then
+                    drawPip(xLeft, yTop, true, true)
+                    drawPip(xLeft, yQtr, true, true)
+                    drawPip(xMid, yMid)
+
+                elseif card.rank == 10 then
+                    drawPip(xLeft, yTop, true, true)
+                    drawPip(xLeft, yQtr, true, true)
+                    drawPip(xMid, 16, false, true)
+                end
+            end
+        end
+
+
+        if  baralhos[indice]:cont() < baralhos[indice]:qte() then 
+            love.graphics.setColor(1, 1, 1)
+            love.graphics.draw(images.card_face_down, 60, 20)
+        end
+
+        drawCard(baralhos[indice]:getCarta(), 2 * 60, 20)
 end
 
 function telaModoJogo()
+    love.graphics.print("MODO JOGO - EM DESENVOLVIMENTO",100,100)
 
 end
 
-function subTelaInformarCarta()
-
+function subTelaInformarCarta(mostrar)
+    -- A 2 3 4 5
+    -- 6 7 8 9 10 
+    -- J Q K
+    -- heart spade club diamond
+     if mostrar == true then 
+         
+     end
 end
 
 function telaPontuacao()
-
+    love.graphics.print("PONTUACAO - EM DESENVOLVIMENTO",100,100)
 end
 
 function telaAjuda()
@@ -67,6 +211,4 @@ function telaDoacao()
     love.graphics.print("BUY ME A COFFEE!",100,100)
 end
 
-function telaReqVideo()
-    love.graphics.print("PATRON ME! WATCH THE AD!",100,100)
-end
+
